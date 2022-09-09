31aef25111288cd80f0155e4471ebcc88f48fa7f
rachit-chaudhary <mailracit@gmail.com>
2022-09-09 13:44:17 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const {MongoClient} = require("mongodb")
 const express = require('express')
 const path = require("path")
 const app = express()
 const port = process.env.PORT || 8080
 
+let db
 
 
 //static files
@@ -19,15 +21,14 @@ app.set('view engine', 'ejs')
 //route for index
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'MPRÉS | Home',
-        message: 'This is ejs'})
+        title: 'MPRÉS | Home'
+    })
 })
 
 //route for about 
 app.get('/about.ejs', (req, res) => {
     res.render('about', {
         title: 'MPRÉS | About',
-        message: 'This is about.ejs',
         heading: 'contacting mprés',
         address: 'C-14, Sector-6, Noida, Uttar Pradesh - 201301',
         email: 'inquiry@mpres.co.in',
@@ -36,10 +37,11 @@ app.get('/about.ejs', (req, res) => {
 })
 
 //route for product 
-app.get('/product.ejs', (req, res) => {
+app.get('/product.ejs', async (req, res) => {
+    const allProducts = await db.collection("products").find().toArray()
     res.render('product', {
         title: 'MPRÉS | Products',
-        message: 'This is product.ejs'
+        allProducts
     })
 })
 
@@ -53,5 +55,13 @@ app.get('/contact.ejs', (req, res) => {
         phone: '+0120 428 6464'
     })
 })
-// listen on port 8080
-app.listen(port, () => console.info(`Listening on port ${port}`))
+
+async function start() {
+    const client = new MongoClient('mongodb+srv://rachit:WW5IO5jbxkRoulor@cluster0.vol2k7z.mongodb.net/MPRES?retryWrites=true&w=majority')
+    await client.connect()
+    db = client.db()
+    // listen on port 8080
+    app.listen(port, () => console.info(`Listening on port ${port}`))
+}
+start()
+
